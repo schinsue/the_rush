726db0255aae8ef0e7e72ac598bd806b18556ca3
Sergio Chin-Sue <s.chinsue@gmail.com>
2022-03-31 13:12:09 +0100

    Ingest and sanitize json

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,53 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     TheRush.Repo.insert!(%TheRush.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule TheRush.Seed.Helper do
+  def sanitize_integer(integer) do
+    {int, _} =
+      "#{integer}"
+      |> String.replace(",", "")
+      |> Integer.parse()
+
+    int
+  end
+
+  def sanitize_float(float) do
+    {float, _} =
+      "#{float}"
+      |> Float.parse()
+
+    float
+  end
+
+  def sanitize_string(string), do: "#{string}"
+end
+
+alias TheRush.Statistics.Player
+alias TheRush.Repo
+alias TheRush.Seed.Helper
+
+# Clean database before seeding
+Repo.delete_all(Player)
+
+# Read json file and insert players
+# Sanitize and create changeset for validation
+File.read!("./priv/repo/rushing.json")
+|> Jason.decode!()
+|> Enum.each(fn player ->
+  %Player{}
+  |> Player.changeset(%{
+    att: Helper.sanitize_integer(player["Att"]),
+    att_g: Helper.sanitize_float(player["Att/G"]),
+    avg: Helper.sanitize_float(player["Avg"]),
+    first_downs: Helper.sanitize_integer(player["1st"]),
+    first_downs_pct: Helper.sanitize_float(player["1st%"]),
+    fourty_yds: Helper.sanitize_integer(player["40+"]),
+    fum: Helper.sanitize_integer(player["FUM"]),
+    lng: Helper.sanitize_string(player["Lng"]),
+    name: Helper.sanitize_string(player["Player"]),
+    position: Helper.sanitize_string(player["Pos"]),
+    td: Helper.sanitize_integer(player["TD"]),
+    team: Helper.sanitize_string(player["Team"]),
+    twenty_yds: Helper.sanitize_integer(player["20+"]),
+    yds: Helper.sanitize_integer(player["Yds"]),
+    yds_g: Helper.sanitize_float(player["Yds/G"])
+  })
+  |> Repo.insert!()
+end)
